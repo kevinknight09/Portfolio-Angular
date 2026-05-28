9ea59049dce61f20071fd873a2550ac32140f0b0
Rounak Saha <saharounak9@gmail.com>
2026-05-28 18:33:35 +0530

    Initial Commit for portfolio website

---
+++ src/app/app.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Footer } from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Hero, About, Skills, Projects, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('portfolio-app');
+}
